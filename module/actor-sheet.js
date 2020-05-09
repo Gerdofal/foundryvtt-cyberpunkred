bbf9caa628d91e5a1de45bcf9b13ed194369a540
cparker <cparker@akbraincenter.com>
2020-05-08 23:29:10 -0800

    Implemented basic clickable skills: Note BUG - Attribute value which should be a portion of the roll is not currently expanding as intended and only shows 0

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -47,6 +47,30 @@ export class SimpleActorSheet extends ActorSheet {
       this.actor.deleteOwnedItem(li.data("itemId"));
       li.slideUp(200, () => this.render(false));
     });
-
+		
+	// Rollable abilities.
+	html.find('.rollable').click(this._onRoll.bind(this));
+	
   }
+	
+  /**
+ * Handle clickable rolls.
+ * @param {Event} event   The originating click event
+ * @private
+ */
+_onRoll(event) {
+  event.preventDefault();
+  const element = event.currentTarget;
+  const dataset = element.dataset;
+
+  if (dataset.roll) {
+    let roll = new Roll(dataset.roll, this.actor.data.data);
+    let label = dataset.label ? `Rolling ${dataset.label}` : '';
+    roll.roll().toMessage({
+      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+      flavor: label
+    });
+  }
+}
+
 }
