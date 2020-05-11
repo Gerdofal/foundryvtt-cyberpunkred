c0d6e09305c67060b5f662c012b6caf4499a7d5e
cparker <cparker@akbraincenter.com>
2020-05-10 20:42:34 -0800

    Removing entries from item sheet which are not needed.

--- module/item-sheet.js
+++ module/item-sheet.js
@@ -20,10 +20,6 @@ export class SimpleItemSheet extends ItemSheet {
   /** @override */
   getData() {
     const data = super.getData();
-    data.dtypes = ["String", "Number", "Boolean"];
-    for ( let attr of Object.values(data.data.attributes) ) {
-      attr.isCheckbox = attr.dtype === "Boolean";
-    }
     return data;
   }
 
