bcf28d7f6963538982e226dbbf5d777b4ac7eca7
cparker <cparker@akbraincenter.com>
2020-05-11 08:06:40 -0800

    Reset of system to use boilerplate

--- module/cyberpunkred.js
+++ module/cyberpunkred.js
@@ -1,19 +1,47 @@
-/**
- * A simple and flexible system for world-building using an arbitrary collection of character and item attributes
- * Author: Atropos
- * Software License: GNU GPLv3
- */
-
 // Import Modules
-import { SimpleActor } from "./actor.js";
-import { SimpleItemSheet } from "./item-sheet.js";
-import { SimpleActorSheet } from "./actor-sheet.js";
+import { BoilerplateActor } from "./actor/actor.js";
+import { BoilerplateActorSheet } from "./actor/actor-sheet.js";
+import { BoilerplateItem } from "./item/item.js";
+import { BoilerplateItemSheet } from "./item/item-sheet.js";
 
-/* -------------------------------------------- */
-/*  Foundry VTT Initialization                  */
-/* -------------------------------------------- */
+Hooks.once('init', async function() {
+  console.log(`Initializing Simple cyberpunkred System`);
+  game.boilerplate = {
+    BoilerplateActor,
+    BoilerplateItem
+  };
 
-Handlebars.registerHelper('if_eq', function(a, b, opts) {
+  /**
+   * Set an initiative formula for the system
+   * @type {String}
+   */
+  CONFIG.Combat.initiative = {
+    formula: "1d20",
+    decimals: 2
+  };
+
+  // Define custom Entity classes
+  CONFIG.Actor.entityClass = BoilerplateActor;
+  CONFIG.Item.entityClass = BoilerplateItem;
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("boilerplate", BoilerplateActorSheet, { makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("boilerplate", BoilerplateItemSheet, { makeDefault: true });
+
+  // If you need to add Handlebars helpers, here are a few useful examples:
+  Handlebars.registerHelper('concat', function() {
+    var outStr = '';
+    for (var arg in arguments) {
+      if (typeof arguments[arg] != 'object') {
+        outStr += arguments[arg];
+      }
+    }
+    return outStr;
+  });
+	
+  Handlebars.registerHelper('if_eq', function(a, b, opts) {
     if (a == b) {
         return opts.fn(this);
     } else {
@@ -21,34 +49,7 @@ Handlebars.registerHelper('if_eq', function(a, b, opts) {
     }
 });
 
-Hooks.once("init", async function() {
-  console.log(`Initializing Simple cyberpunkred System`);
-
-	/**
-	 * Set an initiative formula for the system
-	 * @type {String}
-	 */
-	CONFIG.Combat.initiative = {
-	  formula: "1d10 + @ref",
-    decimals: 2
-  };
-
-	// Define custom Entity classes
-  CONFIG.Actor.entityClass = SimpleActor;
-
-  // Register sheet application classes
-  Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("cyberpunkred", SimpleActorSheet, { makeDefault: true });
-  Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("cyberpunkred", SimpleItemSheet, {makeDefault: true});
-
-  // Register system settings
-  game.settings.register("cyberpunkred", "macroShorthand", {
-    name: "Shortened Macro Syntax",
-    hint: "Enable a shortened macro syntax which allows referencing attributes directly, for example @str instead of @attributes.str.value. Disable this setting if you need the ability to reference the full attribute model, for example @attributes.str.label.",
-    scope: "world",
-    type: Boolean,
-    default: true,
-    config: true
+  Handlebars.registerHelper('toLowerCase', function(str) {
+    return str.toLowerCase();
   });
 });
