41107b1903a4282bee3270b88c9e9fde6acfd03f
cparker <cparker@akbraincenter.com>
2020-05-26 19:10:15 -0800

    Added health and luck calculations

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -53,16 +53,28 @@ export class cyberpunkredActorSheet extends ActorSheet {
     }
     */
     
-    for (let attr of Object.values(data.data.attributes)) {
+    for (let [key, attr] of Object.entries(data.data.attributes)) {
       attr.roll = attr.value + attr.mod;
-      crlog("Calculating Attributes " + attr.value + "+" + attr.mod +  "=" + attr.roll);
+      crlog("Calculating " + key + ": " + attr.value + "+" + attr.mod +  "=" + attr.roll);
     }
     
-    for (let attr of Object.values(data.data.skills)) {
+    for (let [key, attr] of Object.entries(data.data.skills)) {
       attr.roll = attr.value + attr.mod;
-      crlog("Calculating Skills " + attr.value + "+" + attr.mod +  "=" + attr.roll);
+      crlog("Calculating " + key + ": " + attr.value + "+" + attr.mod +  "=" + attr.roll);
+    }    
+    
+    data.data.resources["healthpool"].max = data.data.attributes["body"].roll * 5;
+    crlog("Calculating Health Max " + data.data.attributes["body"].roll + " * 5 = " + data.data.resources["healthpool"].max);
+    if (data.data.resources["healthpool"].max<data.data.resources["healthpool"].value) {
+      data.data.resources["healthpool"].value=data.data.resources["healthpool"].max;
     }
     
+    data.data.resources["luckpool"].max = data.data.attributes["luck"].roll;
+    crlog("Luck Pool Max = " + data.data.attributes["luck"].roll);
+    if (data.data.resources["luckpool"].max<data.data.resources["luckpool"].value) {
+      data.data.resources["luckpool"].value = data.data.resources["luckpool"].max;
+    }
+
     return data;
   }
 
