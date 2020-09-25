c2adb7f1dbd6fcae72b00dddb7fa3c0ae131a109
cparker <cparker@akbraincenter.com>
2020-09-25 08:17:17 -0800

    Back to jsk

--- environment.js
+++ environment.js
@@ -7,8 +7,8 @@ export const environmentSettings = { // Here begins the object
 
   //Note: The settings below are only reloaded when F5 is pressed
 
-  "jsk": false, //Show skills and rules from the jumpstart kit
-  "core": true, //Show skills and rules from the core rulebook for actual play
+  "jsk": true, //Show skills and rules from the jumpstart kit
+  "core": false, //Show skills and rules from the core rulebook for actual play
   "forcemigrate": false //Force a migration of player data. Use with caution.
 
 
