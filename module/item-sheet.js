72acf7ffac021128a668c38539e30932356c333c
Justin May <may.justin@gmail.com>
2021-01-18 11:24:34 -0800

    Update item sheet with new styling

--- module/item-sheet.js
+++ module/item-sheet.js
@@ -21,7 +21,7 @@ export class cyberpunkredItemSheet extends ItemSheet {
   get template() {
     const path = "systems/cyberpunkred/templates/item";
     // Return a single sheet for all item types.
-     return `${path}/item-sheet.html`;
+     return `${path}/item-sheet.hbs`;
 
     // Alternatively, you could use the following return statement to do a
     // unique item sheet by type, like `weapon-sheet.html`.
